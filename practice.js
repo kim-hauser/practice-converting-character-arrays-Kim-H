00e35ef8727217ffe3595b709262d846a427b3b3
kim-hauser <knhauser1s@gmail.com>
2025-09-14 10:23:49 -0500

    Create practice.js
    Initial commit

---
+++ practice.js
@@ -0,0 +1,20 @@
+/*
+Tasks
+Task 1: Decode the Following Reversed Messages
+Use .split(''), reverse.(‘’) and .join('') to convert messages between strings and
+arrays dynamically. Then log the messages.
+1. 2. 3. 4. Message 1: " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks
+gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam
+gnidoC"
+Message 2: "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw
+gnitirw edoc tuB"
+Message 3: "!elpoep rehto morf tnereﬃd on era uoy ,elbuort evah uoy fI
+.lanoisseforp a ekil leef ot evah t'nod uoY"
+Message 4: ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom
+tcefrep eht rof tiaw t'noD .yadot trats tsuJ"
+Task 2: Write your own reverse messages
+1. 2. First, write your own short messages of inspiration (without reversing them).
+Then, use .split(''), reverse.(‘’) and .join('') to convert messages between
+strings and arrays dynamically so that you have a reverse output.
+3. Then log the messages.
+/*
